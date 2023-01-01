a5a66ecb1e9c28c55ab47a2ebc0761671bdc9bc9
Rogerio Vieira <rogeriovbh@gmail.com>
2023-01-01 19:10:11 -0300

    implementa views e urls

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Post
+from django.http import Http404
 
-# Create your views here.
+def post_detail(request, id):
+  post = get_object_or_404(Post, id=id, status=Post.Status.PUBLISHED)
+  return render(request, 'post/detail.html', {'post': post})
+
+def post_list(request):
+  post = Post.published.all()
+  return render(request, 'blog/post/list.html', {'post': post})
