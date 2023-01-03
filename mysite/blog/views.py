068e4618501dedc111a323d055c3cc0a2667ceb5
Rogerio Vieira <rogeriovbh@gmail.com>
2023-01-02 23:17:49 -0300

    corrige arquivo de views e implementa o list.html

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -3,9 +3,9 @@ from .models import Post
 from django.http import Http404
 
 def post_detail(request, id):
-  post = get_object_or_404(Post, id=id, status=Post.Status.PUBLISHED)
-  return render(request, 'post/detail.html', {'post': post})
+  posts = get_object_or_404(Post, id=id, status=Post.Status.PUBLISHED)
+  return render(request, 'post/detail.html', {'posts': posts})
 
 def post_list(request):
-  post = Post.published.all()
-  return render(request, 'blog/post/list.html', {'post': post})
+  posts = Post.published.all()
+  return render(request, 'blog/post/list.html', {'posts': posts})
