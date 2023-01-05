f070007464e88c83557f55df3e50db6d48029807
Rogerio Vieira <rogeriovbh@gmail.com>
2023-01-05 19:08:18 -0300

    implementa SEO-friendly URLs para a aplicacao

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -15,7 +15,7 @@ class Post(models.Model):
 
 
   title = models.CharField(max_length=250)
-  slug = models.SlugField(max_length=250)
+  slug = models.SlugField(max_length=250, unique_for_date='publish')
   author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='blog_post')
   body = models.TextField()
   publish = models.DateTimeField(default=timezone.now)
@@ -36,4 +36,7 @@ class Post(models.Model):
     return self.title
   
   def get_absolute_url(self):
-    return reverse('blog:post_detail', args=[self.id])
+    return reverse('blog:post_detail', args=[self.publish.year,
+                                             self.publish.month,
+                                             self.publish.day,
+                                             self.slug])
