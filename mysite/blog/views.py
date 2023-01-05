8c0d441667b7b21cae7917a73f7ae5ed5948ba9a
Rogerio Vieira <rogeriovbh@gmail.com>
2023-01-05 19:21:20 -0300

    Inicia implementacao da paginacao

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,9 +1,14 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Post
+from django.core.paginator import Paginator
 
 
 def post_list(request):
-    posts = Post.published.all()
+    post_list = Post.published.all()
+    paginator = Paginator(post_list, 3)
+    page_number = request.GET.get('page', 1)
+    posts = paginator.page(page_number)
+    
     return render(request,
                  'blog/post/list.html',
                  {'posts': posts})
