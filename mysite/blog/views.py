a3d38bdd360798c0519241eb9fd7c61cfa02051f
Rogerio Vieira <rogeriovbh@gmail.com>
2023-01-05 21:08:43 -0300

    ïmplementa exception para caso for digitado letras na url como parametro de pagina

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Post
-from django.core.paginator import Paginator, EmptyPage
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 
 def post_list(request):
@@ -9,6 +9,8 @@ def post_list(request):
     page_number = request.GET.get('page', 1)
     try: 
         posts = paginator.page(page_number)
+    except PageNotAnInteger:
+        posts = paginator.page(1)
         
     except EmptyPage:
         
