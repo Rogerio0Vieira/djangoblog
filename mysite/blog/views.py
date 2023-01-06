5d4894bae966874e7d4a193377f075bcde80da2d
Rogerio Vieira <rogeriovbh@gmail.com>
2023-01-05 21:00:43 -0300

    adicionando uma exepion para a paginacao

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,13 +1,18 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Post
-from django.core.paginator import Paginator
+from django.core.paginator import Paginator, EmptyPage
 
 
 def post_list(request):
     post_list = Post.published.all()
     paginator = Paginator(post_list, 3)
     page_number = request.GET.get('page', 1)
-    posts = paginator.page(page_number)
+    try: 
+        posts = paginator.page(page_number)
+        
+    except EmptyPage:
+        
+        posts = paginator.page(paginator.num_pages)
     
     return render(request,
                  'blog/post/list.html',
