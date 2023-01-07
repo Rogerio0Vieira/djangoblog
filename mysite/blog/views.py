d166a854a48c3c14a198e9a0ec329c95db9a03f0
Rogerio Vieira <rogeriovbh@gmail.com>
2023-01-06 21:51:32 -0300

    criando uma class-based view como alternativa a function-based view

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,6 +1,13 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Post
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.views.generic import ListView
+
+class PostListView(ListView):
+    querryset = Post.published.all()
+    context_object_name = 'posts'
+    paginate_by = 3
+    template_name = 'blog/post/list.html'
 
 
 def post_list(request):
