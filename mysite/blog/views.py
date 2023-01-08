862bcbe6c98010f320911a4a17059316f99fdfec
Rogerio Vieira <rogeriovbh@gmail.com>
2023-01-08 20:58:57 -0300

    Implementa o form na view

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -2,6 +2,18 @@ from django.shortcuts import render, get_object_or_404
 from .models import Post
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.views.generic import ListView
+from .forms import EmailPostForm
+
+def post_share(request, post_id):
+    post = get_object_or_404(Post, id=post_id, status=Post.Status.PUBLISHED)
+    if request.method == 'POST':
+        form = EmailPostForm(request.POST)
+        if form.is_valid():
+            cd = form.cleaned_data
+    
+    else:
+        form = EmailPostForm()
+    return render(request, 'blog/post/share.html', {'post': post, 'form': form})
 
 class PostListView(ListView):
     queryset = Post.published.all()
