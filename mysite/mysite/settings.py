e66f01ad4d7ba58c9ead633f0b73a3459eb66dd2
Rogerio Vieira <rogeriovbh@gmail.com>
2023-01-10 15:02:28 -0300

    Implementa envio de email com o django

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -29,11 +29,11 @@ DEBUG = config('DEBUG')
 ALLOWED_HOSTS = []
 
 #Email server configuration
-EMAIL_HOST = ''
-EMAIL_HOST_USER = ''
-EMAIL_HOST_PASSWORD = ''
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
+EMAIL_HOST = config('EMAIL_HOST')
+EMAIL_HOST_USER = config('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
+EMAIL_PORT = config('EMAIL_PORT')
+EMAIL_USE_TLS = config('EMAIL_USE_TLS')
 
 # Application definition
 
