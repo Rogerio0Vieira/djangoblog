077f332ed752ad8b3898f8e6f0bfe207e9805579
Rogerio Vieira <rogeriovbh@gmail.com>
2023-01-16 20:59:51 -0300

    Fix: Ajusta posicao do model para comentarios

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -3,22 +3,6 @@ from django.utils import timezone
 from django.contrib.auth.models import User
 from django.urls import reverse
 
-class Comment(models.Model):
-  post = models.ForeignKey(Post,on_delete=models.CASCADE,related_name='comments')
-  name = models.CharField(max_length=80)
-  email = models.EmailField()
-  body = models.TextField()
-  created = models.DateTimeField(auto_now_add=True)
-  updated = models.DateTimeField(auto_now=True)
-  active = models.BooleanField(default=True)
-  
-  class Meta:
-    ordering = ['created',]
-    indexes = [models.Index(fields=['created']),
-             ]
-  
-  def __str__(self):
-    return f'Comment by {self.name} on {self.post}'
 
 class PublishedManager(models.Manager):
   def get_queryset(self):
@@ -57,3 +41,20 @@ class Post(models.Model):
                                              self.publish.month,
                                              self.publish.day,
                                              self.slug])
+
+class Comment(models.Model):
+  post = models.ForeignKey(Post,on_delete=models.CASCADE,related_name='comments')
+  name = models.CharField(max_length=80)
+  email = models.EmailField()
+  body = models.TextField()
+  created = models.DateTimeField(auto_now_add=True)
+  updated = models.DateTimeField(auto_now=True)
+  active = models.BooleanField(default=True)
+  
+  class Meta:
+    ordering = ['created',]
+    indexes = [models.Index(fields=['created']),
+             ]
+  
+  def __str__(self):
+    return f'Comment by {self.name} on {self.post}'
