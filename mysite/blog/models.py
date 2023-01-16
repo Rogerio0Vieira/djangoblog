24508226f9eaaf1411f1820235756f5382e47302
Rogerio Vieira <rogeriovbh@gmail.com>
2023-01-15 21:05:13 -0300

    implementa no models classe para comentarios

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -3,6 +3,23 @@ from django.utils import timezone
 from django.contrib.auth.models import User
 from django.urls import reverse
 
+class Comment(models.Model):
+  post = models.ForeignKey(Post,on_delete=models.CASCADE,related_name='comments')
+  name = models.CharField(max_length=80)
+  email = models.EmailField()
+  body = models.TextField()
+  created = models.DateTimeField(auto_now_add=True)
+  updated = models.DateTimeField(auto_now=True)
+  active = models.BooleanField(default=True)
+  
+  class Meta:
+    ordering = ['created',]
+    indexes = [models.Index(fields=['created']),
+             ]
+  
+  def __str__(self):
+    return f'Comment by {self.name} on {self.post}'
+
 class PublishedManager(models.Manager):
   def get_queryset(self):
     return super().get_queryset()\
