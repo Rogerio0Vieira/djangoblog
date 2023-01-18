a6785aa18b0f905141e4502628852bafa3e13449
Rogerio Vieira <rogeriovbh@gmail.com>
2023-01-18 20:42:40 -0300

    implementa view para os comentarios

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,11 +1,23 @@
 from django.shortcuts import render, get_object_or_404
-from .models import Post
+from .models import Post, Comment
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.views.generic import ListView
-from .forms import EmailPostForm
+from .forms import EmailPostForm, CommentForm
 from django.core.mail import send_mail
 from decouple import config 
+from django.views.decorators.http import require_POST
 
+
+@require_POST
+def post_comment(request, post_id):
+    post = get_object_or_404(Post, id=post_id, status=Post.Status.PUBLISHED)
+    comment = None
+    form = CommentForm(data=request.POST)
+    if form.is_valid():
+        comment = form.save(commit=False)
+        comment.post = post
+        comment.save()
+    return render(request, 'blog/post/comment.html', {'post':post, 'from':form, 'comment':comment})
 
 def post_share(request, post_id):
     post = get_object_or_404(Post, id=post_id, status=Post.Status.PUBLISHED)
