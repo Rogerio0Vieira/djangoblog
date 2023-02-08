502574299d0aaf322b78073e0f11c369822bce9a
Rogerio Vieira <rogeriovbh@gmail.com>
2023-02-07 23:23:56 -0300

    Corrige identacao

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -13,6 +13,7 @@ def post_comment(request, post_id):
     post = get_object_or_404(Post, id=post_id, status=Post.Status.PUBLISHED)
     comment = None
     form = CommentForm(data=request.POST)
+    
     if form.is_valid():
         comment = form.save(commit=False)
         comment.post = post
